dd6161db51d74b3280bcfac2a654e54dd4670307
Tarikul Islam <tarikulislam@Tarikuls-Mac-mini.local>
2026-03-23 17:58:11 +0600

    Updated Structured

---
+++ assets/js/active.js
@@ -0,0 +1,17 @@
+// =========================================================================================
+// * Project Name :  Eduna - Online Education Courses HTML5 Template.
+// * File         :  JS Base
+// * Version      :  1.0.0
+// * Author       :  BizanTheme (https://themeforest.net/user/bizantheme)
+// * Developer    :  Meheraj Hossain Sagar
+// =========================================================================================
+
+(function ($) {
+  "use strict";
+  $(document).on("ready", function () {
+    /*======================================================================================
+      Nice Select JS
+   =======================================================================================*/
+    $("select").niceSelect();
+  });
+})(jQuery);
